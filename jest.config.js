bf333496087fc5eed2f86f68a6821e69f88ed351
Andrey Smolko <andr.smolko@gmail.com>
2024-01-31 13:11:09 +0100

    fix

--- jest.config.js
+++ jest.config.js
@@ -2,22 +2,16 @@
  * Identity obj proxy is needed for jest to be able to properly import scss files.
  */
 module.exports = {
-    clearMocks: true,
-    coverageReporters: ["lcov"],
-    moduleNameMapper: {
-        "\\.(scss)$": "identity-obj-proxy",
-    },
-    modulePathIgnorePatterns: ["/cypress", ".yalc"],
-    preset: "ts-jest",
-    setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
-    testEnvironment: "jest-environment-jsdom",
-    transform: {
-        "^.+\\.tsx?$": [
-            "ts-jest",
-            {
-                tsconfig: "tsconfig.jest.json",
-                diagnostics: false,
-            },
-        ],
-    },
+  clearMocks: true,
+  coverageReporters: ["lcov"],
+  moduleNameMapper: {
+    "\\.(scss)$": "identity-obj-proxy",
+  },
+  modulePathIgnorePatterns: ["/cypress", ".yalc"],
+  preset: "ts-jest",
+  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest"],
+  },
 };
