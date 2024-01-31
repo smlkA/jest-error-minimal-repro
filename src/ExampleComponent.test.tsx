0a0214dd47d55680dafe9bc8c4fd2f4658e75ba8
Daniel <daanf@live.nl>
2024-01-31 10:13:49 +0100

    initial commit

---
+++ src/ExampleComponent.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { ExampleComponent } from "./ExampleComponent";
+
+describe("ExampleComponent", () => {
+    it("renders", () => {
+        const { getByText } = render(<ExampleComponent />);
+        expect(getByText("Example Component")).toBeInTheDocument();
+    });
+});
